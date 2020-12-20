740aa2806c69715576d749a4dede08b5b40554ac
Yusuf İpek <47244746+Yusufpek@users.noreply.github.com>
2020-12-20 21:28:22 +0300

    Update problem9.cs

--- problem9.cs
+++ problem9.cs
@@ -6,6 +6,15 @@ namespace problem9
     {
         static void Main(string[] args)
         {
+            /*
+            Project Euler Problem 9
+                EN:A Pythagorean triplet is a set of three natural numbers, a < b < c, for which a^2 + b^2 = c^2
+                For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+                There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+                TUR: Bir pisagor üçgeni 3 doğal sayı içeren (a<b<c olacak şekilde a^2+b^2 = c^2 kuralının karşılayan) bir kümedir.
+                a+b+c = 1000 kuralını karşılayan sadece bir üçgen vardır, üçgenin kenarları çarpımını bulun.
+            */
             int a = 2, b = 1;
             double c;
             bool buldukMu = false;
